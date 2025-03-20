005e84ca161941494190b3329fec11d4050a320c
Yoshida <sunnri257@gmail.com>
2025-03-21 08:25:16 +0900

    再修正

--- q6/work/js/app.js
+++ q6/work/js/app.js
@@ -3,6 +3,7 @@ $(function () {
   $(".select-box").on("change", function () {
     // 選択した項目のvalue値を取得して変数select_itemに設定
     const select_item = $(this).val();
+    // リスト全体を非表示
     $(".food-list li").hide();
     // 「全て」またはを変数categoryと同じ項目を選択した場合は表示
     $(".food-list li").filter(function (){
